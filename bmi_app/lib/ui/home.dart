b196036ed2f54e3d003ad72fbf92aca915ad3598
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-12-23 01:44:51 +0530

    Added appbar

---
+++ bmi_app/lib/ui/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+      }
+    
+}
+    
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("BMI"),
+        centerTitle: true,
+        backgroundColor: Colors.pinkAccent,
+      ),
+    );
+  }
+
+}
